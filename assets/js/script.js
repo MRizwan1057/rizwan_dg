4fe5723aef0d9ae638cc1b0cb64a21755aeb971d
MRizwan1057 <96943208+MRizwan1057@users.noreply.github.com>
2024-05-11 02:08:40 +0500

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,104 @@
+$(document).ready(function () {
+  // business logic
+  console.log("chala777");
+  $(".h_slider").slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 3500,
+          centerMode: true,
+          centerPadding: "40px",
+        },
+      },
+    ],
+  });
+  $(".p_slider").slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });
+
+  $(".s_slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "60px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  $(".b_slider").slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2200,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });
+});
